fceeaadb0e6ba67f3c7769285f2068932bfdc040
yoayana1231 <yo.ayana1231@gmail.com>
2026-06-04 00:40:46 +0900

    wip: お気に入りEntity Repository

---
+++ src/main/java/jp/co/sss/shop/controller/FavoriteController.java
@@ -0,0 +1,14 @@
+package jp.co.sss.shop.controller;
+
+import org.springframework.stereotype.Controller;
+
+/*
+ * お気に入り機能のコントローラクラス
+ */
+
+@Controller
+public class FavoriteController {
+	
+	
+
+}
