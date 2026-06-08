a1907acd2902838fa07e93f3ca2ae620126c8187
masakifootball <ms-ishida@a-c-t.co.jp>
2026-06-08 17:51:31 +0900

    あいまい検索の実装をしました

--- src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -1,7 +1,5 @@
 package jp.co.sss.shop.controller.client.item;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.repository.ItemRepository;
 import jp.co.sss.shop.service.BeanTools;
 
@@ -38,23 +35,33 @@ public class ClientItemShowController {
 	 * @param model    Viewとの値受渡し
 	 * @return "index" トップ画面
 	 */
-	@RequestMapping(path = "/" , method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(path = "/", method = { RequestMethod.GET, RequestMethod.POST })
 	public String index(Model model) {
 		model.addAttribute("items", itemRepository.findAll());
 		return "index";
 	}
 
-//	売れてない奴も表示する（商品一覧）
+	//	売れてない奴も表示する（商品一覧）
 	@RequestMapping(path = "/client/item/list/{sortType}", method = { RequestMethod.GET })
-	public String clientItem(@PathVariable int sortType,Model model) {
+	public String clientItem(@PathVariable int sortType, Model model) {
 		model.addAttribute("items", itemRepository.findAllByQuantityDesc());
-//		新着順
-		if (sortType ==1) {
+		//		新着順
+		if (sortType == 1) {
 			model.addAttribute("items", itemRepository.findAllByOrderByInsertDateDesc());
-//		売れ筋順	
+			//		売れ筋順	
 		} else {
-			model.addAttribute("items",itemRepository.findAllByQuantityDesc());
+			model.addAttribute("items", itemRepository.findAllByQuantityDesc());
 		}
+		return "client/item/list";
+	}
+
+	//石田実装 あいまい検索用コントローラー
+	//name属性 search
+	//新規追加リポジトリメソッド findByNameContaining
+
+	@RequestMapping("/client/item/list/search")
+	public String clientItemListSearch(String search, Model model) {
+		model.addAttribute("items", itemRepository.findByNameContaining(search));
 		return "client/item/list";
 	}
 }
