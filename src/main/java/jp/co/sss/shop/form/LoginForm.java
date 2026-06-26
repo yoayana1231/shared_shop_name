a625e64d1edfa00fbe4aa02ae989a80db79d80f6
yoayana1231 <yo.ayana1231@gmail.com>
2026-06-26 09:39:14 +0900

    メールアドレスのバグ修正

--- src/main/java/jp/co/sss/shop/form/LoginForm.java
+++ src/main/java/jp/co/sss/shop/form/LoginForm.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
 import jp.co.sss.shop.annotation.LoginCheck;
 
 /**
@@ -21,6 +20,7 @@ public class LoginForm implements Serializable {
 	 * メールアドレス
 	 */
 	@NotBlank
+	@Pattern(regexp = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\\\.[a-zA-Z0-9-.]+$")
 	@Email
 	private String email;
 
